95fc0a0d654c7d19ed70e2fff98ce47bd0d2cb87
unknown <imtiaz.nafes@gmail.com>
2022-02-12 23:43:42 +0600

    modification

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="mt-2">
-<div class="relative flex items-center justify-center ">
-    <h2>Search By User</h2>
+<div class="flex items-center justify-center ">
+    <h2 class="text-black font-serif m-8">Search By User</h2>
     <div class="flex border-2 border-gray-200 rounded">
         <input v-model="searchWord" type="text" class="px-4 py-2 w-80 text-black" placeholder="Search...">
         <button v-on:click="search()" class="px-4 text-white bg-gray-600 border-l ">
